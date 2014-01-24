34e4126ffa12d9a7798eac6198945de9c72ecc8c
BCleverly <ben.cleverly@outlook.com>
2014-01-24 17:32:18 +0000

    refactor
    refactor

--- js/main.js
+++ js/main.js
@@ -1,22 +1,53 @@
 if (!$.support.transition)
   $.fn.transition = $.fn.animate;
 
-$lb = $('.login-box');
+var $lb = $('.login-box');
+var docHeight = Math.floor( $(document).height() / 2 );
+var lfHeight = Math.floor( $lb.height() );
+var transHeight = docHeight-lfHeight;
 
-$('.start-anim').on('click', function(e){
-	
-	var docHeight = Math.floor($(document).height() /2);
-	var lfHeight = Math.floor($lb.height() /2);
-	var transHeight = docHeight-lfHeight;
-	
-	console.log(docHeight, lfHeight)
-	
-	$lb.show(function()
-	{
-		$(this).transit({
-			y: transHeight
-		});
-	});
-	
+$('.start-anim-fade').on('click', function(e){
+	fadeInLoginForm();			
 	return false;
 });
+
+$('.start-anim-slide').on('click', function(e){
+	slideInLoginForm();			
+	return false;
+});
+
+function slideInLoginForm()
+{
+	
+}
+
+function fadeInLoginForm()
+{
+	if(checkFormStatus())
+	{
+		$lb.find('.login-form').fadeOut(function(){
+			$lb.transit({
+				top:0,
+				y: '-250px'
+			})
+			.fadeOut()
+			.data('active', false);
+		});
+	}
+	else 
+	{
+		$lb.show(function(){
+			$(this).transit({
+				top:'auto',
+				y: transHeight
+			}, function(){
+				$(this).find('.login-form').fadeIn();
+			});		
+		}).data('active', true);
+	}
+}
+
+function checkFormStatus()
+{
+	return $lb.data('active');
+}
