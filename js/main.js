a038406ee0d8dc98a4421dac3f044e18f3658bb8
BCleverly <ben.cleverly@outlook.com>
2014-01-24 16:47:26 +0000

    positon play
    haha

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
+if (!$.support.transition)
+  $.fn.transition = $.fn.animate;
+
 $lb = $('.login-box');
 
 $('.start-anim').on('click', function(e){
 	
-	var docHeight = $(document).height();
-	transHeight = (docHeight / 2) - ($lb.height() / 2);
+	var docHeight = Math.floor($(document).height() /2);
+	var lfHeight = Math.floor($lb.height() /2);
+	var transHeight = docHeight-lfHeight;
 	
-	console.log(docHeight, transHeight)
+	console.log(docHeight, lfHeight)
 	
-	$lb.show(function(){
+	$lb.show(function()
+	{
 		$(this).transit({
 			y: transHeight
 		});
